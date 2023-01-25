28c84870e78be224e6fc0c86fc1dcfa9eda92974
norwinz <91311240+norwinz@users.noreply.github.com>
2023-01-25 16:51:19 +0100

    Delete testScripts.ps1

--- testScripts.ps1
+++
@@ -1,7 +0,0 @@
-$nameManage = "AD01"
-                $usernameTemp = "tempAdmin"
-                $password = ConvertTo-SecureString "Sommar2020" -AsPlainText -Force
-                $credTemp = new-object -typename System.Management.Automation.PSCredential -argumentlist $usernameTemp, $password
-                Invoke-Command  -VMname $nameManage -Credential $credTemp -ScriptBlock {
-                    get-windowsfeature AD-Domain-Services
-                }
